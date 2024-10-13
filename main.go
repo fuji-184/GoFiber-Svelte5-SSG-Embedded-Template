27935ce0956585210a8f1ff81bd6a8a6bf38c8ed
Fuji <fujisantoso134@gmail.com>
2024-10-13 07:10:23 +0700

    updated

--- main.go
+++ main.go
@@ -1,36 +1,94 @@
 package main
 
 import (
-	"github.com/gofiber/fiber/v2"
+	"database/sql"
 	"embed"
 	"io/fs"
+	"log"
 	"net/http"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/filesystem"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 //go:embed all:svelte/build
 var embedDirStatic embed.FS
 
+type Tes struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
 func main() {
 	app := fiber.New()
 
 	s, err := fs.Sub(embedDirStatic, "svelte/build")
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
 	staticServer := filesystem.New(filesystem.Config{
 		Root: http.FS(s),
 	})
 
+	db, err := sql.Open("sqlite3", "./fuji.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(5)
+	db.SetConnMaxLifetime(time.Hour)
+
+	defer db.Close()
+
+	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS tes (id INTEGER NOT NULL PRIMARY KEY, name TEXT)`)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stmt, err := db.Prepare("INSERT INTO tes(name) VALUES(?)")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec("fuji")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	app.Get("/", staticServer)
 	app.Get("/_app/*", staticServer)
 	app.Get("/service-worker.js", staticServer)
-    app.Get("/favicon.png", staticServer)
+	app.Get("/favicon.png", staticServer)
+	app.Get("/tes", func(c *fiber.Ctx) error {
+		return handleTes(c, db)
+	})
 	app.Get("/*", func(c *fiber.Ctx) error {
 		c.Path("/")
 		return staticServer(c)
 	})
 
-	app.Listen(":8000")
+	app.Listen(":3000")
+}
+
+func handleTes(c *fiber.Ctx, db *sql.DB) error {
+	rows, err := db.Query("SELECT id, name FROM tes")
+	if err != nil {
+		return c.Status(http.StatusInternalServerError).SendString("Error querying data")
+	}
+	defer rows.Close()
+
+	var results []Tes
+	for rows.Next() {
+		var tes Tes
+		if err := rows.Scan(&tes.Id, &tes.Name); err != nil {
+			return c.Status(http.StatusInternalServerError).SendString("Error scanning row")
+		}
+		results = append(results, tes)
+	}
+
+	return c.JSON(results)
 }
